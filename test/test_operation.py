dfe7543b93ad3d5943affa68c9eaf009e06ee0fc
jitender-insights <kashyap.jitender09@gmail.com>
2025-01-08 10:40:51 +0530

    unit test cases updated

---
+++ test/test_operation.py
@@ -0,0 +1,11 @@
+from src.maths_operation import add , sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(3,3) == 6
+    assert add(-2,3) == 1
+    
+def test_sub():
+    assert sub(3,2) == 1
+    assert sub(4,2) == 2
+    assert sub(-3,2) == -1
